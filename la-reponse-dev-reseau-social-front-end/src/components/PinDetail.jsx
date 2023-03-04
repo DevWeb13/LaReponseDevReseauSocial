8a5593818c81ca4b58dc78855f44a49598c87c4c
DevWeb13 <giuliano.loic@sfr.fr>
2023-03-04 22:00:00 +0100

    refactor: (PinDetails.jsx): delete import

--- la-reponse-dev-reseau-social-front-end/src/components/PinDetail.jsx
+++ la-reponse-dev-reseau-social-front-end/src/components/PinDetail.jsx
@@ -7,7 +7,6 @@ import { client, urlFor } from '../client';
 import MasonryLayout from './MasonryLayout';
 import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
 import Spinner from './Spinner';
-import uuid from 'uuid';
 
 /**
  *
